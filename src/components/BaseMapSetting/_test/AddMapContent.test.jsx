36cf8d913c444d7d6b0e3d7336834b7fd3de4bf0
ripanrenaldi25 <ripanrenaldi25042002@gmail.com>
2023-06-08 15:54:35 +0700

    make test fail

--- src/components/BaseMapSetting/_test/AddMapContent.test.jsx
+++ src/components/BaseMapSetting/_test/AddMapContent.test.jsx
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import AddMapContent from '../AddMapContent';
-// import NavigationLink from '../../NavigationLink';
-// import OnlineSource from '../../InputWeb/OnlineSource';
-// import '@testing-library/jest-dom';
-// import userEvent, { render, screen } from '@testing-library/react';
+import React from 'react';
+import AddMapContent from '../AddMapContent';
+import NavigationLink from '../../NavigationLink';
+import OnlineSource from '../../InputWeb/OnlineSource';
+import '@testing-library/jest-dom';
+import userEvent, { render, screen } from '@testing-library/react';
 
-// describe('<Add Map Content /> Component', () => {
-//   it('Should render correctly', () => {
-//     render(<AddMapContent sourceComponent={<OnlineSource />} title="Add Online Source" onAddHandler={() => {}} setOptions={[]} />);
-//     const title = screen.getByRole('heading', {
-//       level: 1,
-//     });
-//     const titleText = screen.getByText('Add Online Source');
+describe('<Add Map Content /> Component', () => {
+  it('Should render correctly', () => {
+    render(<AddMapContent sourceComponent={<OnlineSource />} title="Add Online Source" onAddHandler={() => {}} setOptions={[]} />);
+    const title = screen.getByRole('heading', {
+      level: 1,
+    });
+    const titleText = screen.getByText('Add Online Source');
 
-//     const navigationButtonsLink = screen.getAllByRole('button');
-//     navigationButtonsLink.forEach((navLink) => {
-//       expect(navLink).toBeInTheDocument();
-//     });
+    const navigationButtonsLink = screen.getAllByRole('button');
+    navigationButtonsLink.forEach((navLink) => {
+      expect(navLink).toBeInTheDocument();
+    });
 
-//     expect(title).toBeInTheDocument();
-//     expect(titleText).toBeInTheDocument();
-//   });
-// });
+    expect(title).toBeInTheDocument();
+    expect(titleText).toBeInTheDocument();
+  });
+});
 
 it('asd', () => {
   expect(true).toBeTruthy();
