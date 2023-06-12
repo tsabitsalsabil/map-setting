d2e63696a6be6b5d036f778fdac7f42b14837646
tsabitsalsabil <tsabitsalsabil@gmail.com>
2023-06-12 15:16:08 +0700

    style(categories): change style from base map categories component

--- src/components/BaseMapSetting/BaseMapCategories.jsx
+++ src/components/BaseMapSetting/BaseMapCategories.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 
+
 function BaseMapCategories() {
+  const [selectedOption, setSelectedOption] = useState(' ');
+  const [isOpen, setIsOpen] = useState(false);
+  const handleChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+  const toggleDropdown =()=>{
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <section className="bg-white w-2/12 h-12 flex justify-between items-center px-4 rounded-xl border shadow-md shadow-[rgba(0,0,0,.2)] cursor-pointer">
-      <span>All Categories</span>
-      <span>
-        <IoIosArrowDown />
-      </span>
-    </section>
+ <div className="relative inline-block w-2/12">
+      <select
+        value={selectedOption}
+        onChange={handleChange}
+        className="appearance-none py-2 pl-6 pr-8 bg-white= border border-gray-300 rounded-xl w-full h-12 shadow-md shadow-[rgba(0,0,0,.2)] text-lg font-thin"
+        onClick={toggleDropdown}
+      >
+        <option value="">All categories</option>
+        <option value="option1">Map</option>
+        <option value="option2">Source</option>
+      </select>
+      <div className={`text-2xl pointer-events-none absolute inset-y-0 right-1 flex items-center px-2 text-gray-700 transition-transform duration-300 ${isOpen ? 'transform rotate-180' : ''}`} >
+        <span>
+          <i><IoIosArrowDown /></i>
+        </span>
+      </div>
+    </div>
   );
 }
 
